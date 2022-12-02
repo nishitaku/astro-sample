c8423d4a8f07ed0c27ae1906c807952b29a6a9f5
nishitaku <29690066+nishitaku@users.noreply.github.com>
2022-12-02 11:29:42 +0900

    add CouterVue.vue

---
+++ src/components/CounterVue.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from "vue";
+
+const count = ref(0);
+</script>
+<template>
+  <h1>Vue Counter</h1>
+  <div>{{ count }}</div>
+  <div>
+    <button @click="count++">increment</button>
+    <button @click="count--">decrement</button>
+  </div>
+</template>
